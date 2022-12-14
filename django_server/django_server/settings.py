2d416ede97200fe7e59662ac4f42344b772fa234
VynoDePal <83235998+VynoDePal@users.noreply.github.com>
2022-12-14 11:57:57 +0100

    first commit memoire licence 2022

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -140,9 +140,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 INTERNAL_IPS = ['127.0.0.1']
 
